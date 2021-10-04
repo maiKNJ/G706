6ebe86dc6e1bde2c0d195315a466b515e6a98a86
maiKNJ <43847780+maiKNJ@users.noreply.github.com>
2021-10-04 14:18:24 +0200

    server

---
+++ Server/Server/Client.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Server
+{
+    class Client
+    {
+        public static int dataBufferSize = 4096;
+        public int id;
+        public TCP tcp;
+
+        public Client(int _clientId)
+        {
+            id = _clientId;
+            tcp = new TCP(id);
+        }
+
+        public class TCP
+        {
+            public TcpClient socket;
+
+            private readonly int id;
+            private NetworkStream stream;
+            private byte[] recieveBuffer;
+            public TCP(int _id)
+            {
+                id = _id;
+            }
+
+            public void Connect(TcpClient _socket)
+            {
+                socket = _socket;
+                socket.ReceiveBufferSize = dataBufferSize;
+                socket.SendBufferSize = dataBufferSize;
+
+                stream = socket.GetStream();
+
+                recieveBuffer = new byte[dataBufferSize];
+
+                stream.BeginRead(recieveBuffer, 0, dataBufferSize, ReceiveCallback, null);
+                //TODO : send welcome packet
+            }
+
+            private void ReceiveCallback(IAsyncResult _result)
+            {
+                try
+                {
+                    int _byteLength = stream.EndRead(_result);
+                    if (_byteLength <= 0)
+                    {
+                        //TODO: disconnect
+                        return;
+                    }
+                    byte[] _data = new byte[_byteLength];
+                    Array.Copy(recieveBuffer, _data, _byteLength);
+
+                    //TODO: handle data
+                    stream.BeginRead(recieveBuffer, 0, dataBufferSize, ReceiveCallback, null);
+                }
+                catch (Exception _ex)
+                {
+
+                   Console.WriteLine($"Error recieving TCP data: {_ex}");
+                    //TODO: disconnect
+                }
+            }
+        }
+    }
+}
---
+++ Server/Server/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Server";
+            MyServer.start(50, 26950);
+
+            Console.ReadKey();
+        }
+    }
+}
